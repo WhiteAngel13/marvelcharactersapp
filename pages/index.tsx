79e5ce9bf7366c8f0c65d0449602d51b047e20c1
White Angel <62609991+WhiteAngel13@users.noreply.github.com>
2021-08-21 14:56:55 -0400

    Instaled all Libs and SimpleConfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import { GetServerSideProps } from "next"
+import { api } from "../services/api"
+
 export default function Home() {
   return (
     <div>
       <h1>Contrata ai pow, site mó legal</h1>
     </div>
   )
+}
+
+export const getServerSideProps:GetServerSideProps = async(ctx) => {
+
+  const { data } = await api.get("characters")
+
+  return {
+    props: {}
+  }
 }
---
+++ services/api.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { createHash } from "crypto";
+
+const ts = new Date().getTime();
+const apikey = process.env.MARVEL_PUBLIC_APIKEY;
+const secretKey = process.env.MARVEL_SECRET_APIKEY;
+const hash = createHash("md5").update(ts + secretKey + apikey).digest("hex");
+
+export const api = axios.create({
+  baseURL:"http://gateway.marvel.com/v1/public",
+  params:{
+    ts,
+    apikey,
+    hash
+  }
+})
+
