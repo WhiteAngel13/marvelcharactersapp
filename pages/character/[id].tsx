efce5889ff90884ec1a908b6267b25bf0eb4e11f
White Angel <62609991+WhiteAngel13@users.noreply.github.com>
2021-08-24 22:39:13 -0400

    SSG with new Keys

--- pages/character/[id].tsx
+++ pages/character/[id].tsx
@@ -15,24 +15,24 @@ interface CharacterPageProps {
   heroData: CharacterData
 }
 
-// export const getStaticPaths : GetStaticPaths = async (ctx) => {
+export const getStaticPaths : GetStaticPaths = async (ctx) => {
 
-//   const herosData : HomeHerosType[] = [];
+  const herosData : HomeHerosType[] = [];
 
-//   const response = await db.collection("Home_Heros_Data").get()
-//   response.forEach(item => herosData.push(item.data() as HomeHerosType))
+  const response = await db.collection("Home_Heros_Data").get()
+  response.forEach(item => herosData.push(item.data() as HomeHerosType))
 
-//   const paths = herosData.map(hero => {
-//     return {
-//       params: {id: String(hero.id)}
-//     }
-//   })
+  const paths = herosData.map(hero => {
+    return {
+      params: {id: String(hero.id)}
+    }
+  })
 
-//   return {
-//     paths,
-//     fallback: "blocking"
-//   }
-// }
+  return {
+    paths,
+    fallback: "blocking"
+  }
+}
 
 export default function CharacterPage ({
   heroData
@@ -71,13 +71,14 @@ export default function CharacterPage ({
   )
 }
 
-export const getServerSideProps : GetServerSideProps = async(ctx) => {
+export const getStaticProps : GetStaticProps = async(ctx) => {
 
   const { id } = ctx.params;
 
   try {
     const { data } = await api.get(`characters/${id}`)
 
+    console.log(data)
 
     const heroData = await PageCharacter_HeroDataFormatter(data.data.results[0]);
 
@@ -87,8 +88,6 @@ export const getServerSideProps : GetServerSideProps = async(ctx) => {
       },
     }
   }catch (err){
-
-
     return {
       notFound: true
     }
